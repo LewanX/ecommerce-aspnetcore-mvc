b328bc8380e72ee551c3c06eb35b840c0dda2914
Alan Jason Lewandowski <85626827+LewanX@users.noreply.github.com>
2023-06-30 18:36:35 -0300

    Vistas actores y productores

---
+++ eTickets/Controllers/ActorsController.cs
@@ -0,0 +1,21 @@
+﻿using eTickets.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly AppDbContext _context;
+        public ActorsController(AppDbContext context)
+        {
+            _context=context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            //var data=_context.Actors.ToList();
+
+            return View(await _context.Actors.ToListAsync());
+        }
+    }
+}
