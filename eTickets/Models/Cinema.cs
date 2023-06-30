5f072a06831305693cb0efb6e2c5ab3c390fc4b8
Alan Jason Lewandowski <85626827+LewanX@users.noreply.github.com>
2023-06-30 20:39:00 -0300

    -cinema view

--- eTickets/Models/Cinema.cs
+++ eTickets/Models/Cinema.cs
@@ -6,8 +6,11 @@ namespace eTickets.Models
     {
         [Key]
         public int Id { get; set; }
+        [Display(Name = "Cinema logo")]
         public string Logo { get; set; }
+        [Display(Name = "Name")]
         public string Name { get; set; }
+        [Display(Name = "Description")]
         public string Description { get; set; } 
         //Relationships
         public List<Movie> Movies { get; set; }
